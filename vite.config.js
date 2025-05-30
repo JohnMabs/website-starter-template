c82c9edcbd0fb6fb303040b5e6711de494c28e8c
John Mabiala <johnmabiala@hotmail.com>
2025-05-30 08:00:18 +0100

    fix some stuff

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import htmlPurge from "vite-plugin-purgecss";
 
 export default defineConfig({
   root: resolve(__dirname, "src/"),
